25f306289ef6f0f1ae6761c8fbeac8973259d22c
Diogo Martins <antdiogofmartins@gmail.com>
2018-08-30 20:16:36 -0300

    Histogram Equalization

--- image_filters.py
+++ image_filters.py
@@ -4,12 +4,41 @@ import matplotlib.pyplot as plt
 from math import *
 
 
+def image_histogram(im_name, im):
+    data = im.copy().flatten()
+    plt.hist(data, 256)
+    plt.savefig('histograms/histogram_' + im_name)
+
+
+def normalized_histogram(im):
+    h = [0.0] * 256
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            h[im[i, j]] += 1
+    return np.array(h)/(im.shape[0]*im.shape[1])
+
+
+def histogram_equalization(im_name, im):
+    normalized_hist = normalized_histogram(im)
+    cdf = normalized_hist.cumsum()
+    transfer_function_values = np.uint8(255 * cdf)
+    equalized_im = im.copy()
+
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            equalized_im[i][j] = transfer_function_values[im[i][j]]
+
+    imageio.imwrite('post_processed_images/eq_' + im_name, im)
+    image_histogram('eq_' + im_name, equalized_im)
+
+
 def negative(im_name, im):
     for i in range(im.shape[0]):
         for j in range(im.shape[1]):
             neg_byte = 255 - im[i][j]
             im[i][j] = neg_byte
     imageio.imwrite('post_processed_images/negative_' + im_name, im)
+    image_histogram('negative_' + im_name, im)
 
 
 def logarithmic(im_name, im):
@@ -18,6 +47,7 @@ def logarithmic(im_name, im):
             log_byte = (255/log(256)) * log(1 + im[i][j])
             im[i][j] = log_byte
     imageio.imwrite('post_processed_images/log_' + im_name, im)
+    image_histogram('log_' + im_name, im)
 
 
 def gamma_correction(im_name, im, exponent):
@@ -26,6 +56,7 @@ def gamma_correction(im_name, im, exponent):
             gamma_byte = (255/(256 ** exponent)) * ((1 + im[i][j]) ** exponent)
             im[i][j] = gamma_byte
     imageio.imwrite('post_processed_images/gamma_' + im_name, im)
+    image_histogram('gamma_' + im_name, im)
 
 
 def bit_layers(im_name, im):
@@ -57,6 +88,15 @@ def bit_layers(im_name, im):
     imageio.imwrite('post_processed_images/bitlayer5_' + im_name, layer_5_im)
     imageio.imwrite('post_processed_images/bitlayer6_' + im_name, layer_6_im)
     imageio.imwrite('post_processed_images/bitlayer7_' + im_name, layer_7_im)
+
+    image_histogram('bitlayer0_' + im_name, layer_0_im)
+    image_histogram('bitlayer1_' + im_name, layer_1_im)
+    image_histogram('bitlayer2_' + im_name, layer_2_im)
+    image_histogram('bitlayer3_' + im_name, layer_3_im)
+    image_histogram('bitlayer4_' + im_name, layer_4_im)
+    image_histogram('bitlayer5_' + im_name, layer_5_im)
+    image_histogram('bitlayer6_' + im_name, layer_6_im)
+    image_histogram('bitlayer7_' + im_name, layer_7_im)
 
 
 def piecewise_linear(im_name, im, x_0_1, y_0_1, x_1_1, y_1_1, x_0_2, y_0_2, x_1_2, y_1_2, x_0_3, y_0_3, x_1_3, y_1_3):
